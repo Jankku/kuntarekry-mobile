92bfebe63079e3adc1d4b91f1ecbc33795e7f38e
Jankku <29043938+Jankku@users.noreply.github.com>
2023-01-16 12:36:12 +0200

    Load job ad list

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text } from "react-native";
+import { API_URL, API_CLIENT } from "@env";
 
 export default function App() {
+  const [jobAds, setJobAds] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const url = new URL("/portal-api/recruitment/open-jobs", API_URL);
+      url.searchParams.append("client", API_CLIENT);
+      const res = await fetch(url.toString());
+      const json = await res.json();
+      setJobAds(json.jobAdvertisements);
+    })();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      {jobAds !== undefined ? (
+        <FlatList
+          data={jobAds}
+          renderItem={({ item }) => (
+            <Text key={item.jobAdvertisement.id}>
+              {item.jobAdvertisement.id} - {item.jobAdvertisement.title}
+            </Text>
+          )}
+        />
+      ) : null}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
