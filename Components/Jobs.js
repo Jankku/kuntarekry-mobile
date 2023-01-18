6307e7e37caad55364561a04a7d8d47ef119eb64
Haatis <joona00@windowslive.com>
2023-01-18 15:27:58 +0200

    Job page pagination work in progress

--- Components/Jobs.js
+++ Components/Jobs.js
@@ -1,14 +1,41 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
 
 export default function Jobs({ data }) {
+  const [minIndex, setMinIndex] = useState(0);
+  const [maxIndex, setMaxIndex] = useState(10);
+  const Maxpages = Math.ceil(data.length / 10);
+
+  const onPageForward = () => {
+    if (Maxpages > 1 + minIndex / 10) {
+      setMinIndex(minIndex + 10);
+      setMaxIndex(maxIndex + 10);
+    }
+  };
+  const onPageBack = () => {
+    if (1 + minIndex / 10 > 1) {
+      setMinIndex(minIndex - 10);
+      setMaxIndex(maxIndex - 10);
+    }
+  };
   return (
-    <FlatList
-      data={data}
-      ListEmptyComponent={<Text>Ei työpaikkailmoituksia</Text>}
-      renderItem={JobItem}
-      keyExtractor={(_, index) => index}
-      ItemSeparatorComponent={ListItemSeparator(16)}
-    />
+    <>
+      <FlatList
+        data={data.slice(minIndex, maxIndex)}
+        ListEmptyComponent={<Text>Ei työpaikkailmoituksia</Text>}
+        renderItem={JobItem}
+        keyExtractor={(_, index) => index}
+        ItemSeparatorComponent={ListItemSeparator(16)}
+      />
+      <View>
+        <Text>
+          Page {1 + minIndex / 10} / {Maxpages}
+        </Text>
+        <Text onPress={onPageForward}>Seuraava</Text>
+        <Text onPress={onPageBack}>Takaisin</Text>
+      </View>
+    </>
   );
 }
 
